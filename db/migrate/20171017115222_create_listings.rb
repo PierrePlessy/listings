9c1ed10e089b98f8d389e144695031883129a13b
Julien Grand-Chavin <julien.grandchavin@icloud.com>
2017-10-17 14:08:22 +0200

    Add in db: messages, categories, comments, listings

---
+++ db/migrate/20171017115222_create_listings.rb
@@ -0,0 +1,11 @@
+class CreateListings < ActiveRecord::Migration[5.1]
+  def change
+    create_table :listings do |t|
+        t.timestamps
+        t.string :title
+        t.text :description
+        t.string :picture
+        t.decimal :price
+    end
+  end
+end
