91cb34b01f7a80b5aa0aced9c8e2b621b07145de
PierrePlessy <pierre.plessy@outlook.fr>
2017-10-18 17:11:09 +0200

    Merge master

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,8 +4,6 @@ class Listing < ApplicationRecord
   belongs_to :category
   belongs_to :user
 
-  has_many :comments
-
   def self.search(args)
     Listing.where('title LIKE :query', query: "%#{args[:keywords]}%")
   end
